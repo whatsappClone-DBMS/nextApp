41b734751f8ddc0cccbbae2a93fee517acec2c23
avivashishta29 <avivashishta@gmail.com>
2022-03-19 04:55:30 +0530

    final

--- Components/ChatView/MessageBox.js
+++ Components/ChatView/MessageBox.js
@@ -1,19 +1,18 @@
-import React from 'react'
+import React from "react";
 import styles from "./styles.module.css";
-import SendIcon from '@mui/icons-material/Send';
-
+import SendIcon from "@mui/icons-material/Send";
 
 function MessageBox() {
   return (
     <div className={styles.messageContainer}>
-        <input className={styles.textMessage}
-        style={{marginLeft:"1rem"}}
+      <input
+        className={styles.textMessage}
+        style={{ marginRight: "2rem" }}
         placeholder="Type a message"
-        >
-        </input>
-        <SendIcon sx={{marginRight:"2rem"}}/>
+      ></input>
+      <SendIcon sx={{ marginRight: "2rem" }} />
     </div>
-  )
+  );
 }
 
-export default MessageBox
+export default MessageBox;
