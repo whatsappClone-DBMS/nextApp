4f169819a6b45d9ffe240e216e06771959a81282
avivashishta29 <avivashishta@gmail.com>
2022-03-19 04:08:18 +0530

    changes

--- Components/ChatView/ChatView.js
+++ Components/ChatView/ChatView.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ChatViewHeader from "./ChatViewHeader";
-
+import styles from "./styles.module.css";
 function ChatView() {
-  return(
-    <div>
-      <ChatViewHeader/>
+  return (
+    <div className={styles.ChatViewContainer}>
+      <ChatViewHeader />
     </div>
-  )
+  );
 }
 
 export default ChatView;
--- Components/ChatView/ChatViewHeader.js
+++ Components/ChatView/ChatViewHeader.js
@@ -1,29 +1,36 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React from "react";
+import styles from "./styles.module.css";
 import { Avatar } from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-
+import SearchIcon from "@mui/icons-material/Search";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import IconButton from "@mui/material/IconButton";
 
 function ChatViewHeader() {
   return (
     <div className={styles.container}>
-        <div style={{display:"flex"}}>
-            <Avatar
-            alt="Remy Sharp"
-            src="http://sc01.alicdn.com/kf/HTB1jA_RXrj1gK0jSZFuq6ArHpXab.jpg"
-            style={{ marginRight: 15 }}/>
+      <div style={{ display: "flex" }}>
+        <Avatar
+          alt="Remy Sharp"
+          src="http://sc01.alicdn.com/kf/HTB1jA_RXrj1gK0jSZFuq6ArHpXab.jpg"
+          style={{ marginRight: 15 }}
+        />
         <div>
-            <h1 style={{margin:"0"}}>Aryan Teng</h1>
-            <p style={{margin:"0", fontSize:"0.8rem"}}>online</p>
+          <h1 style={{ margin: "0" }}>Aryan Teng</h1>
+          <p style={{ margin: "0", fontSize: "0.8rem" }}>online</p>
         </div>
-        </div>
-        <div style={{display:"flex", marginRight:"2rem", alignItems:"center"}}>
-            <SearchIcon/>
-            <MoreVertIcon/>
-        </div>
+      </div>
+      <div
+        style={{ display: "flex", marginRight: "2rem", alignItems: "center" }}
+      >
+        <IconButton style={{ color: "#AEBAC1" }}>
+          <SearchIcon />
+        </IconButton>
+        <IconButton style={{ color: "#AEBAC1" }}>
+          <MoreVertIcon />
+        </IconButton>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ChatViewHeader
+export default ChatViewHeader;
