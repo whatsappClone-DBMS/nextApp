14581aeb9a70061327afbbdcbce195f154cdc3d4
avivashishta29 <avivashishta@gmail.com>
2022-03-19 04:14:41 +0530

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/assets/Icon.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import Header from '../Components/Header/Header'
-import LogIn from '../Components/LogIn/LogIn'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Header from "../Components/Header/Header";
+import LogIn from "../Components/LogIn/LogIn";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Header/>
-      <LogIn/>
+      <Header />
+      <LogIn />
     </div>
-  )
+  );
 }
