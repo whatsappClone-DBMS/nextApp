6ad9a4e738150e2ed8b0b8274bf83aaad55daf52
aryanteng <aryanteng892@gmail.com>
2022-03-19 01:31:10 +0530

    first

---
+++ Components/LogIn/LogIn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+
+function LogIn() {
+  return (
+    <div>LogIn</div>
+  )
+}
+
+export default LogIn
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import LogIn from '../Components/LogIn/LogIn'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      
+      <LogIn/>
     </div>
   )
 }
