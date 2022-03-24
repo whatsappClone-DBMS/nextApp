9fe07c8a1a14ccd5626419972d7536452e089b72
aryanteng <aryanteng892@gmail.com>
2022-03-25 01:53:23 +0530

    stories completed

--- Components/Story/StoryComponent.js
+++ Components/Story/StoryComponent.js
@@ -9,7 +9,7 @@ function StoryComponent() {
           alt="Remy Sharp"
           src={"http://sc01.alicdn.com/kf/HTB1jA_RXrj1gK0jSZFuq6ArHpXab.jpg"}
           sx={{ width: 40, height: 40, cursor: "pointer" }}/>
-      <div className={styles.nameFlex}>
+      <div className={styles.nameFlex} style={{marginLeft:"0.5rem"}}>
         <p className={styles.name}>Aryan Teng</p>
         <p className={styles.message}>today at 10:20 pm</p>
       </div>
