eb5b7400f4c246b70aee98e9eac708f3a58c7fdd
avivashishta29 <avivashishta@gmail.com>
2022-03-28 00:10:40 +0530

    profile

--- pages/profile.js
+++ pages/profile.js
@@ -10,7 +10,21 @@ export default function ProfilePage() {
   return (
     <div className={styles.container}>
       <Profile uid={uid} />
-      <ChatView dmId={dmId} uid={uid} />
+      {dmId ? (
+        <ChatView dmId={dmId ?? -1000} uid={uid} />
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "70vw",
+            backgroundColor: "#212E35",
+          }}
+        >
+          <img src="assets/DefaultChat.svg" style={{ maxWidth: "450px" }} />
+        </div>
+      )}
     </div>
   );
 }
