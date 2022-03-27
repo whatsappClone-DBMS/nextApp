51ebae41ded801726950513671549ee17c6ffa81
aryanteng <aryanteng892@gmail.com>
2022-03-28 00:07:37 +0530

    blah

--- pages/home.js
+++ pages/home.js
@@ -4,6 +4,7 @@ import styles from "../styles/Home2.module.css";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import ContactInfo from "../Components/ContactInfo/ContactInfo";
+import { display } from "@mui/system";
 
 export default function HomeAfterLogin() {
   const router = useRouter();
@@ -15,9 +16,17 @@ export default function HomeAfterLogin() {
       {dmId?
       (<ChatView dmId={dmId ?? -1000} uid={uid} />)
       :
-      <div></div>
-
-
+      <div style={{
+        display:"flex", 
+        alignItems:"center", 
+        justifyContent:"center", 
+        width:"70vw", 
+        backgroundColor:"#212E35"}}>
+        <img 
+        src="assets/DefaultChat.svg"
+        style={{maxWidth:"450px"}}
+        />
+      </div>
       }
     </div>
   );
