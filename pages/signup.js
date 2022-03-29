0418405b7ec08b88fac3761fcd7136d4ad0ce41a
avivashishta29 <avivashishta@gmail.com>
2022-03-29 23:25:18 +0530

    signup fix

--- pages/signup.js
+++ pages/signup.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import Header from '../Components/Header/Header'
-import SignUp from '../Components/SignUp/SignUp'
+import React from "react";
+import Header from "../Components/Header/Header";
+import SignUp from "../Components/SignUp/SignUp";
+import styles from "../styles/Home.module.css";
 
 function signupPage() {
   return (
     <div>
-        <Header/>
-        <SignUp/>
+      <Header />
+      <SignUp />
     </div>
-  )
+  );
 }
 
-export default signupPage
+export default signupPage;
