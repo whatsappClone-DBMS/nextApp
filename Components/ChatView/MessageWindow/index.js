730547602b661177acec4b89e3d71046a6ebcc57
avivashishta29 <avivashishta@gmail.com>
2022-03-30 02:34:00 +0530

    am pm

--- Components/ChatView/MessageWindow/index.js
+++ Components/ChatView/MessageWindow/index.js
@@ -35,19 +35,13 @@ function MessageWindow({ dmId, user }) {
     }
   }, [dmId]);
 
-  function tConvert(time) {
-    // Check correct time format and split into components
-    time = time
-      .toString()
-      .match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
-
-    if (time.length > 1) {
-      // If time format correct
-      time = time.slice(1); // Remove full string match value
-      time[5] = +time[0] < 12 ? "AM" : "PM"; // Set AM/PM
-      time[0] = +time[0] % 12 || 12; // Adjust hours
-    }
-    return time.join(""); // return adjusted time or original string
+  function tConvert(timeString) {
+    var hourEnd = timeString.indexOf(":");
+    var H = +timeString.substr(0, hourEnd);
+    var h = H % 12 || 12;
+    var ampm = H < 12 ? "AM" : "PM";
+    timeString = h + timeString.substr(hourEnd, 3) + ampm;
+    return timeString;
   }
 
   return (
