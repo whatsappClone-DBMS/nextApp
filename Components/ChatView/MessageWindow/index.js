47f0752da3bd24052ea0d841e5b6ebf91ff0e2ae
aryanteng <aryanteng892@gmail.com>
2022-03-30 02:38:58 +0530

    k

--- Components/ChatView/MessageWindow/index.js
+++ Components/ChatView/MessageWindow/index.js
@@ -39,7 +39,7 @@ function MessageWindow({ dmId, user }) {
     var hourEnd = timeString.indexOf(":");
     var H = +timeString.substr(0, hourEnd);
     var h = H % 12 || 12;
-    var ampm = H < 12 ? "AM" : "PM";
+    var ampm = H < 12 ? " AM" : " PM";
     timeString = h + timeString.substr(hourEnd, 3) + ampm;
     return timeString;
   }
