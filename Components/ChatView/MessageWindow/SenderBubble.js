098d1677fe80f4059c988e9aac47f2effd3c904b
avivashishta29 <avivashishta@gmail.com>
2022-04-09 01:05:47 +0530

    sender bubble delete

--- Components/ChatView/MessageWindow/SenderBubble.js
+++ Components/ChatView/MessageWindow/SenderBubble.js
@@ -8,6 +8,18 @@ function SenderBubble({ message, time }) {
       <span style={{ marginLeft: 10, fontSize: "0.55rem", opacity: 0.7 }}>
         {time}
       </span>
+      <span className={styles.dropdown}>
+        <IconButton style={{ color: "#AEBAC1" }}>
+          <KeyboardArrowDownIcon
+            style={{ marginRight: 10, fontSize: "0.55rem", opacity: 0.7 }}
+          />
+          <div className={styles.dropdownContent}>
+            <Link href={`/settings?uid=${uid}&dmId=${dmId}`}>
+              Delete For Everyone
+            </Link>
+          </div>
+        </IconButton>
+      </span>
     </div>
   );
 }
